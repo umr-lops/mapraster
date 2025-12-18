a0edeec2b5fc1cc9543cc709d53537ed0b2bda8e
agrouaze <antoine.grouazel@ifremer.fr>
2025-12-18 10:09:49 +0000

    second add from cookie-cutter

---
+++ mapraster/__init__.py
@@ -0,0 +1,19 @@
+# type: ignore[attr-defined]
+"""mapraster is a Python lib to interpolate xarray raster field on image geometry (e.g. line/sample)"""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
